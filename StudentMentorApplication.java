35662b87d8d1e4c2303058c5f212467950faa256
srivani vinnakota <164530944+sv410@users.noreply.github.com>
2024-11-20 21:42:31 +0530

    Create StudentMentorApplication.java

---
+++ StudentMentorApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class StudentMentorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMentorApplication.class, args);
+    }
+}
