3643b13483af33f2cb6f584576662fa88916a39f
srivani vinnakota <164530944+sv410@users.noreply.github.com>
2024-11-20 21:43:45 +0530

    Create UserProfile.java

---
+++ UserProfile.java
@@ -0,0 +1,10 @@
+@Entity
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    // Getters and Setters
+}
